e5ba01d7c2360c3473b22d6dd68e3fc52c0fa471
admin <admin@haker-sergey>
2016-04-15 18:09:06 +0300

    work commit #2 at work add button to work with 2 pane

--- src/tutorialfx/JavaFXDifferentThings.java
+++ src/tutorialfx/JavaFXDifferentThings.java
@@ -1,64 +1,3 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package javafxdifferentthings;
-//
-//import javafx.application.Application;
-//import static javafx.application.Application.launch;
-//import javafx.event.ActionEvent;
-//import javafx.event.EventHandler;
-//import javafx.geometry.Pos;
-//import javafx.scene.Scene;
-//import javafx.scene.control.Button;
-//import javafx.scene.control.Label;
-//import javafx.scene.control.PasswordField;
-//import javafx.scene.control.TextField;
-//import javafx.scene.layout.BorderPane;
-//import javafx.scene.layout.HBox;
-//import javafx.scene.layout.Pane;
-//import javafx.scene.layout.StackPane;
-//import javafx.scene.layout.VBox;
-//import javafx.scene.paint.Color;
-//import javafx.stage.Stage;
-//
-///**
-// *
-// * @author sergey
-// */
-//public class JavaFXDifferentThings extends Application {
-//    
-//    @Override
-//    public void start(Stage primaryStage) {
-//        Button btn = new Button();
-//        btn.setText("Say 'Hello World'");
-//        btn.setOnAction(new EventHandler<ActionEvent>() {
-//            
-//            @Override
-//            public void handle(ActionEvent event) {
-//                System.out.println("Hello World!");
-//            }
-//        });
-//        
-//        StackPane root = new StackPane();
-//        root.getChildren().add(btn);
-//        
-//        Scene scene = new Scene(root, 300, 250);
-//        
-//        primaryStage.setTitle("Hello World!");
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-//    }
-//
-//    /**
-//     * @param args the command line arguments
-//     */
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//    
-//}
 package tutorialfx;
 
 import javafx.application.Application;
@@ -69,47 +8,58 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 public class JavaFXDifferentThings extends Application {
+
+    private Button btnOKEdit;
+    private Button btnOK;
+    private Button btnEdit;
+    private Button btnCancel;
+    private Stage stage;
+    private Scene scene1;
+    private Scene scene2;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         primaryStage.setTitle("Login");
         primaryStage.setWidth(400);
-        primaryStage.setHeight(150);
+        primaryStage.setHeight(250);
         primaryStage.setMaxWidth(400);
-        primaryStage.setMaxHeight(150);
+        primaryStage.setMaxHeight(250);
+        stage = primaryStage;
 
-        BorderPane root = new BorderPane();
-        
+//        BorderPane root = new BorderPane();
+        Pane root = new Pane();
 
-        Button btn1 = addButton("OK");
-        Button btn2 = addButton("Edit");
-        Button btn3 = addButton("Cancel");
-        Button btn4 = addButton("OK");
+        btnOK = addButton("OK");
+        btnEdit = addButton("Edit");
+        btnCancel = addButton("Cancel");
+        btnOKEdit = addButton("OK");
 
-        btn3.setOnMousePressed(event -> {
+        btnCancel.setOnMousePressed(event -> {
             primaryStage.close();
         });
 
-        Label label1 = addLabel("USER");
-        Label label2 = addLabel("PASSWORD");
-        Label label3 = addLabel("OLD PASSWORD");
-        Label label4 = addLabel("NEW PASSWORD");
-        Label label5 = addLabel("CONFIRM NEW PASSWORD");
+        Label labelUSER = addLabel("USER");
+        Label labelPASSWORD = addLabel("PASSWORD");
+        Label labelOLD_PASSWORD = addLabel("OLD PASSWORD");
+        Label labelNEW_PASSWORD = addLabel("NEW PASSWORD");
+        Label labelCONFIRM_PASSWORD = addLabel("CONFIRM PASSWORD");
 
-        HBox hboxButton = new HBox(15);
-        HBox hboxUser = new HBox(15);
-        HBox hboxPass = new HBox(15);
+        HBox hboxButton = new HBox(60);
+        HBox hboxUser = new HBox(60);
+        HBox hboxPass = new HBox(60);
 
         VBox vboxPassUser = new VBox(15);
 
@@ -117,11 +67,10 @@ public class JavaFXDifferentThings extends Application {
         TextField textField2 = addTextField();
         TextField textField3 = addTextField();
         TextField textField4 = addTextField();
-        
-        
+
         PasswordField passwordField2 = addPasswordField();
 
-        btn1.setOnMousePressed(event -> {
+        btnOK.setOnMousePressed(event -> {
             if ((!textField1.getText().equals("sergey"))
                     || (!passwordField2.getText().equals("1234"))) {
                 System.out.println("Your UserName or password is incorrect!");
@@ -131,60 +80,92 @@ public class JavaFXDifferentThings extends Application {
             textField1.clear();
             passwordField2.clear();
         });
-        btn2.setOnAction(new EventHandler<ActionEvent>() {
+        btnEdit.setOnAction(new EventHandler<ActionEvent>() {
+
             @Override
             public void handle(ActionEvent event) {
                 primaryStage.setTitle("Edit Password");
                 primaryStage.setWidth(400);
-                primaryStage.setHeight(150);
+                primaryStage.setHeight(250);
                 primaryStage.setMaxWidth(400);
-                primaryStage.setMaxHeight(150);
-                
-                HBox hboxOldPass = new HBox(15);
-                HBox hboxNewPass = new HBox(15);
-                HBox hboxConfNewPass = new HBox(15);
-                hboxOldPass.getChildren().addAll(label3, textField2);
-                hboxNewPass.getChildren().addAll(label4, textField3);
-                hboxConfNewPass.getChildren().addAll(label5, textField4);
-                
-                
+                primaryStage.setMaxHeight(250);
+
+                HBox hboxOldPass = new HBox(60);
+                HBox hboxNewPass = new HBox(60);
+                HBox hboxConfNewPass = new HBox(60);
+                HBox hboxButton = new HBox(30);
+
+                hboxOldPass.getChildren().addAll(labelOLD_PASSWORD, textField2);
+                hboxOldPass.setTranslateX(10);
+                hboxOldPass.setTranslateY(10);
+
+                hboxNewPass.getChildren().addAll(labelNEW_PASSWORD, textField3);
+                hboxNewPass.setTranslateX(10);
+                hboxNewPass.setTranslateY(40);
+
+                hboxConfNewPass.getChildren().addAll(labelCONFIRM_PASSWORD, textField4);
+                hboxConfNewPass.setTranslateX(10);
+                hboxConfNewPass.setTranslateY(70);
+
+                hboxButton.getChildren().addAll(btnOKEdit, btnCancel);
+                hboxButton.setTranslateX(70);
+                hboxButton.setTranslateY(120);
+
                 Pane root2 = new Pane();
-                
-                
-                
-                
-                
-                
-                root2.getChildren().addAll(hboxOldPass, hboxNewPass, hboxConfNewPass, btn4, btn3);
-                Scene scene2 = new Scene(root2);
-                primaryStage.setScene(scene2);
+
+                root2.getChildren().addAll(hboxOldPass, hboxNewPass, hboxConfNewPass, hboxButton);
+                scene2 = new Scene(root2);
+//                primaryStage.setScene(scene2);
+                stage.setScene(scene2);
             }
         });
 
-        btn4.setOnMousePressed( event -> {
-                
-//            root.st
-                    
-                    
-        });
+        btnOKEdit.setOnAction(e -> HandleButtonAction(e));
+
+//        VGap vgap = new VGap();
         
-        hboxUser.getChildren().addAll(label1, textField1);
-        hboxPass.getChildren().addAll(label2, passwordField2);
+    final Popup popup = new Popup(); popup.setX(300); popup.setY(200);
+//    popup.getContent().addAll(new Circle(25, 25, 50, Color.AQUAMARINE));
+    
 
-        vboxPassUser.getChildren().addAll(hboxUser, hboxPass);
-        vboxPassUser.setSpacing(85);
+popup.getContent().addAll();
 
-        hboxButton.getChildren().addAll(btn1, btn2, btn3);
+    Button show = new Button("About Author");
+    
+    show.setOnAction(new EventHandler<ActionEvent>() {
+      @Override public void handle(ActionEvent event) {
+        popup.show(primaryStage);
+      }
+    });
+        
+        
+        hboxUser.getChildren().addAll(labelUSER, textField1);
+        hboxUser.setTranslateX(10);
+        hboxUser.setTranslateY(10);
+        hboxPass.getChildren().addAll(labelPASSWORD, passwordField2);
+        hboxPass.setTranslateX(10);
+        hboxPass.setTranslateY(40);
+//        vboxPassUser.getChildren().addAll(hboxUser, hboxPass);
+//        vboxPassUser.setSpacing(85);
+
+        hboxButton.getChildren().addAll(btnOK, btnEdit, btnCancel);
+        hboxButton.setTranslateX(10);
+        hboxButton.setTranslateY(80);
 
         /*
-        BorderPane 
+                BorderPane
          */
-        root.setTop(hboxUser);
-        root.setCenter(hboxPass);
-        root.setBottom(hboxButton);
+//        root.setTop(hboxUser);
+//        root.setCenter(hboxPass);
+//        root.setBottom(hboxButton);
 
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
+        /*
+                Pane
+         */
+        root.getChildren().addAll(hboxUser, hboxPass, hboxButton,show);
+
+        scene1 = new Scene(root);
+        primaryStage.setScene(scene1);
         primaryStage.show();
 
     }
@@ -195,7 +176,7 @@ public class JavaFXDifferentThings extends Application {
 
         btn.setOnMouseEntered(event
                 -> {
-            btn.setRotate(15);
+            btn.setRotate(0);
             btn.setScaleX(1.1);
             btn.setScaleY(1.1);
             btn.setText(name);
@@ -213,9 +194,10 @@ public class JavaFXDifferentThings extends Application {
 
     private Label addLabel(String name) {
         Label label = new Label(name);
-        label.setPrefSize(70, 20);
+        label.setPrefSize(170, 20);
         label.setTextFill(Color.web("#0076a3"));
-        label.setAlignment(Pos.CENTER);
+        label.setFont(Font.font("Cambria", 14));
+        label.setAlignment(Pos.CENTER_LEFT);
         return label;
     }
 
@@ -229,12 +211,24 @@ public class JavaFXDifferentThings extends Application {
     private PasswordField addPasswordField() {
         PasswordField passwordField = new PasswordField();
         passwordField.setPrefSize(120, 20);
-        passwordField.setPromptText("Your password");
+        passwordField.setPromptText("Enter your password");
 //        textField.setAlignment(Pos.CENTER);
         return passwordField;
+    }
+
+    private void HandleButtonAction(ActionEvent event) {
+
+        if (event.getTarget() == btnOKEdit) {
+            stage.setScene(scene1);
+        }
+//     else{
+//         stage.setScene(scene1);
+//     }
+
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
